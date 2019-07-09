772bb4dafe3c0eb4efea5f07ba465fb3dd862e28
Shelby Sweeney <shelby@cloudistics.com>
2019-07-09 13:33:40 -0400

    populated dropdown directive

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+// This allows the dropdown to be opened/closed by clicking the dropdown button
+import { Directive, HostListener, HostBinding } from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') toggleOpen() {
+    this.isOpen = !this.isOpen;
+  }
 
   constructor() { }
 
 }
+
+// // This would allow the dropdown to be closed by clicking anywhere outside of the dropdown list
+// import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
+//
+// @Directive({
+//   selector: '[appDropdown]'
+// })
+// export class DropdownDirective {
+//   @HostBinding('class.open') isOpen = false;
+//   @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+//     this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+//   }
+//   constructor(private elRef: ElementRef) {}
+// }
